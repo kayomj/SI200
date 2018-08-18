a74016e3376b0c97a929ac50fca2d38e667229aa
kayomj <kayomj@gmail.com>
2018-08-17 21:34:10 -0300

    Update and rename Aluno.c to Aluno_Ex3.c

--- Aluno.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-#include "ex3.h"
-Aluno * newAluno(char * nome, int ra){
-Aluno *aux = Aluno*malloc(sizeof(Aluno));
-aux
---
+++ Aluno_Ex3.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include "ex3.h"
+Aluno * newAluno(char * nome, int ra){}
+Aluno * AddNota(int maxnota){
+  Aluno *aux = (Aluno*)malloc(sizeof(Aluno));
+  aux->nota=(int*)malloc(sizeof(int*(maxnota);
+  
