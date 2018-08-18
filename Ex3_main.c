cfaf2f83af9c1b8c650eb00247202279b29e0969
kayomj <kayomj@gmail.com>
2018-08-17 22:07:29 -0300

    dados insridos e impressão de dados

--- Ex3_main.c
+++ Ex3_main.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 #include "Aluno_Ex3.c"
 #include "Aluno_Ex3.h"
-
+#include "Dados.h"
+#include "Dados.c"
 int main(){
-  Aluno*newAluno (121122, Pato Donald);
+  Aluno *newAluno (123456, Pato Donald);
+  Aluno *add(Pato Donald,80);
+  Aluno *add(Pato Donald,90);
+  Aluno *add(Pato Donald,100);
+  Aluno *newAluno (123333,Margarida);
+  Aluno *add(Margarida,90);
+  Aluno *add(Margarida,90);
+  Aluno *add(Margarida,100);
+  imorime(Margarida);
   imprimeD(Pato Donald);
   
 return 0;
