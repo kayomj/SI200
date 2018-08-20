9773e0985956f60d9c2bf98c882fa7edc51ceb44
kayomj <kayomj@gmail.com>
2018-08-20 19:28:28 -0300

    Correcoe_pt1

--- Ex3_main.c
+++ Ex3_main.c
@@ -8,13 +8,13 @@
 #include "Aluno_Ex3.h"
 int main(){
   Aluno *newAluno (123456, Pato Donald);
-  Aluno *add(Pato Donald,80);
-  Aluno *add(Pato Donald,90);
-  Aluno *add(Pato Donald,100);
+  Aluno *AddNota(Pato Donald,80);
+  Aluno *AddNota(Pato Donald,90);
+  Aluno *AddNota(Pato Donald,100);
   Aluno *newAluno (123333,Margarida);
-  Aluno *add(Margarida,90);
-  Aluno *add(Margarida,90);
-  Aluno *add(Margarida,100);
+  Aluno *AddNota(Margarida,90);
+  Aluno *AddNota(Margarida,90);
+  Aluno *AddNota(Margarida,100);
   imorime(Margarida);
   imprimeD(Pato Donald);
   
