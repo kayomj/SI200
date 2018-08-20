c6350b7cbe0905e92f83a7f356734de8d8a5c868
kayomj <kayomj@gmail.com>
2018-08-20 19:52:51 -0300

    Aspas nos nomes

--- Ex3_main.c
+++ Ex3_main.c
@@ -7,14 +7,14 @@
 #include "Aluno_Ex3.c"
 #include "Aluno_Ex3.h"
 int main(){
-  Aluno *newAluno (123456, Pato Donald);
-  Aluno *AddNota(Pato Donald,80);
-  Aluno *AddNota(Pato Donald,90);
-  Aluno *AddNota(Pato Donald,100);
-  Aluno *newAluno (123333,Margarida);
-  Aluno *AddNota(Margarida,90);
-  Aluno *AddNota(Margarida,90);
-  Aluno *AddNota(Margarida,100);
+  Aluno *newAluno (123456, "Pato Donald");
+  Aluno *AddNota("Pato Donald",80);
+  Aluno *AddNota("Pato Donald",90);
+  Aluno *AddNota("Pato Donald",100);
+  Aluno *newAluno (123333,"Margarida");
+  Aluno *AddNota("Margarida",90);
+  Aluno *AddNota("Margarida,90);
+  Aluno *AddNota("Margarida",100);
   imorime(Margarida);
   imprimeD(Pato Donald);
   
