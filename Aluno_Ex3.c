eecdcb6401495c80c6cb10da0f8368d6f3933fef
kayomj <kayomj@gmail.com>
2018-08-20 20:25:14 -0300

    Endereço

--- Aluno_Ex3.c
+++ Aluno_Ex3.c
@@ -6,12 +6,12 @@ int ra;
 };
 Aluno * newAluno(int ra, char * nome){
   Aluno *aux = (Aluno*)malloc(sizeof(Aluno));
-  aux->nome=nome;
-  aux->ra=ra;
-  aux->notas=newDados(3);
+  aux->Alnuo.nome=nome;
+  aux->Aluno.ra=ra;
+  aux->Aluno.notas=newDados(3);
   
 }
 Aluno * AddNota(Aluno *aux,int maxnota){
-  add(aux->notas, maxnota);
+  add(aux->Aluno.notas, maxnota);
   return aux;
 }
