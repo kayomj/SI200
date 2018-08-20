f17718afc529a20670026a695c970c5b956cab57
kayomj <kayomj@gmail.com>
2018-08-20 20:53:11 -0300

    Update Aluno_Ex3.c

--- Aluno_Ex3.c
+++ Aluno_Ex3.c
@@ -6,9 +6,9 @@ int ra;
 };
 Aluno * newAluno(int ra, char * nome){
   Aluno *aux = (Aluno*)malloc(sizeof(Aluno));
-  aux.nome->Aluno.nome;
-  aux.ra->Aluno.ra;
-  aux.notas->Aluno.newDados(3);
+  aux->nome=Aluno.nome;
+  aux->ra=Aluno.ra;
+  aux->notas=Aluno.newDados(3);
   
 }
 Aluno * AddNota(Aluno *aux,int maxnota){
