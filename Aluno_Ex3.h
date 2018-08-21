cb1054f65ca42b01ac3a4e1f35307e174bcf5414
kayomj <kayomj@gmail.com>
2018-08-20 21:25:40 -0300

    Update Aluno_Ex3.h

--- Aluno_Ex3.h
+++ Aluno_Ex3.h
@@ -8,8 +8,8 @@ int ra;
 int notas;
 Dados * nota;
 }Aluno;
-void Aluno * newAluno(int ra, char * nome[100]);
-void Aluno * AddNota(Aluno *aux,int maxnota);
+void Aluno* newAluno(int ra, char * nome[100]);
+void Aluno* AddNota(Aluno *aux,int maxnota);
 void ImprimeAluno(Aluno *a);
 
 #endif
