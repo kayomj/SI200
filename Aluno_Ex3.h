70780634966d0145339da3347c6757529d2810e9
kayomj <kayomj@gmail.com>
2018-08-20 21:20:16 -0300

    Update Aluno_Ex3.h

--- Aluno_Ex3.h
+++ Aluno_Ex3.h
@@ -1,4 +1,15 @@
-typedef struct Aluno Aluno;
-Dados * notas;
-Aluno * newAluno(int ra, char * nome[100]);
-Aluno * AddNota(Aluno *aux,int maxnota);
+#ifndef ALUNO_H
+#define ALUNO_H
+
+typedef struct Aluno {
+char nome[100];
+int ra;
+int notas;
+Dados * nota;
+}Aluno;
+void Aluno * newAluno(int ra, char * nome[100]);
+void Aluno * AddNota(Aluno *aux,int maxnota);
+void ImprimeAluno(Aluno *a);
+
+#endif
+
